42e789782653233d7b07c8423ae78e8c2e361d72
Gabriel cuadros <gabrielcuadros@gmail.com>
2023-10-27 13:08:22 +0000

    sync

--- Sampleclass.cs
+++ Sampleclass.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace mydependency
 {
-    public static class Class1
+    public static class Sampleclass
     {
         public static bool StartsWithUpper(this string str)
         {
